50c34beeba36d16e0bd6bf16a9fb33a35c6974fd
Sergio Gonzalez <sgonzalezlopez@gmail.com>
2022-12-08 09:09:16 +0100

    Libreria inputmask

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ module.exports.configureRoutes = (app) => {
 }
 
 module.exports.configureStatic = (app) => {
+  app.use('/inputmask', express.static(path.join(__dirname, '../node_modules/inputmask')));
   app.use('/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap')));
   app.use('/bootstrap-table', express.static(path.join(__dirname, '../node_modules/bootstrap-table/dist')));
   app.use('/mdi', express.static(path.join(__dirname, '../node_modules/@mdi/font')));
